ca978511abd29f9ad39a0c21b6cb0ac75fad0224
valenzuelag24 <Jesus.ValenzuelaGelvez@teleperformance.com>
2025-06-06 18:54:20 -0500

    se integro los cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import controlador from './controlador/controlador.js';
-
+import cors from 'cors'
 
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use('/', controlador);
 
